90f305d6236c53159ee5c80931f04efec6d82c21
owouwu42 <BradyOL20@student.cbhs.school.nz>
2022-08-11 11:08:25 +1200

    v1.7 update
    v1.7 patch notes:
    - Bug fixes:
    - Fixed bug where all messages ran at the beginning
    - New Features:
    - added in multiple question numbers for future development
    - Bugs Still To Overcome:
    - questions function not running
    - still printing placeholder "qu" variable at start as  a result of the questions function not running

--- quiz_mechanics.py
+++ quiz_mechanics.py
@@ -1,11 +1,4 @@
 qa=0
-qu=("ERROR")
-
-def questions(): 
-    if questionnum==1:
-        qu=("which option contains the letter a?")
-        qa=q1
-
 #question answers defined
 q1=3
 q2=1
@@ -13,16 +6,58 @@ q3=4
 q4=4
 q5=2
 
+qu=("")
+
 questionnum=1
 points=0
 answer_list=[1,2,3,4]
 
+def questions(): 
+    if questionnum==1:
+        qu=("which option contains the letter a?")
+        qa=q1
+        opt=("1. "
+             "2. "
+             "3. "
+             "4. ")
+    elif questionnum==2:
+        qu=("")
+        qa=q2
+        opt=("1. "
+             "2. "
+             "3. "
+             "4. ")
+    elif questionnum==3:
+        qu=("")
+        qa=q3
+        opt=("1. "
+             "2. "
+             "3. "
+             "4. ")
+    elif questionnum==4:
+        qu=("")
+        qa=q4
+        opt=("1. "
+             "2. "
+             "3. "
+             "4. ")
+    elif questionnum==5:
+        qu=("")
+        qa=q4
+        opt=("1. "
+             "2. "
+             "3. "
+             "4. ")
+
+
 #answer error message defined
-anserror=print("please enter 1, 2, 3, or 4")
-win=print("~~~~CORRECT~~~~         Points: ", (points))
-loss=print("~~~~Wrong~~~~ Better luck next time!   The correct answer was: ", (qa), "   Points: ", (points))
-while questionnum<6:
-    exec(questions)
+anserror=("please enter 1, 2, 3, or 4")
+win=("~~~~CORRECT~~~~         Points: ", (points))
+loss=('~~~~Wrong~~~~ Better luck next time!   The correct answer was: ', (qa), '   Points: ', (points))
+
+questions()
+while questionnum<=6:
+    questions()
     print(qu)
     ans=input()
     if ans==1 or 2 or 3 or 4:
@@ -35,3 +70,4 @@ while questionnum<6:
             questionnum=questionnum+1
     else:
         print(anserror)
+        
