b3c873bf69a352274db3cf54c898d5a9d0762174
owouwu42 <98989986+owouwu42@users.noreply.github.com>
2022-08-24 22:18:15 +1200

    v1.9 update
    v1.19 patch notes:
    - ADDED IN QUESTIONS
    - New quiz questions tab
    - Fully annotated quiz mechanics tab
    - Fully annotated quiz questions tab
    - Now fully functional
    - About to be added to main code

--- Quiz_questions.py
+++ Quiz_questions.py
@@ -1,5 +1,5 @@
 import math
-#question answers defined
+#question answers defined 
 q1="3"
 q2="1"
 q3="4"
@@ -21,48 +21,45 @@ def questions_and_answers():
     #setting the vriables defined within the function to be global so that python shell still accepts them as defined variables.
     #setting initial value for the question which would start as question 1
     global question
-    question=("which option contains the letter a?")
-    #setting the answer to the answer for question 1
+    question=("Which game number is Skyrim in the Elder Scrolls series?")
+    #setting the answer to the answer (qa) for question 1 by pulling it from the previously defined answer numbers (q1)
     global qa
     qa=q1
-    #defining the question 1 answer options
+    #defining the question 1 answer options (opt)
     global opt
-    opt=("1.c    "
-         "2.c    "
-         "3.a    "
-         "4.c    ")        
+    opt=("1. Elder Scrolls III        2. Elder Scrolls IV        3. Elder Scrolls V        4. Elder Scrolls VI")        
     #checking which question the player is on and changing the question, answer, and answer options appropriately 
     if questionnum==2:
-        question=("which option is option 1?")
+        question=("Who was the head developer, producer, writer, and designer of The Elder Scrolls V: Skyrim?")
         qa=q2
-        opt=("1. "
-             "2. "
-             "3. "
-             "4. ")
+        opt=("1. Todd Howard        "
+             "2. Jeremy Soule        "
+             "3. Emil Pagliarulo        "
+             "4. Kurt Kuhlmann")
     #checking which question the player is on and changing the question, answer, and answer options appropriately
     elif questionnum==3:
-        question=("the answer is 4")
+        question=("What year was Skyrim released?")
         qa=q3
-        opt=("1. "
-             "2. "
-             "3. "
-             "4. ")
+        opt=("1. 2013        "
+             "2. 2018        "
+             "3. 2016        "
+             "4. 2011")
     #checking which question the player is on and changing the question, answer, and answer options appropriately
     elif questionnum==4:
-        question=("it's still 4")
+        question=("What illicit drug are the Khajiit of Skyrim well-known for selling")
         qa=q4
-        opt=("1. "
-             "2. "
-             "3. "
-             "4. ")
+        opt=("1. Giant water        "
+             "2. Wabbajack        "
+             "3. Groundwater hit        "
+             "4. Skooma")
     #checking which question the player is on and changing the question, answer, and answer options appropriately 
     elif questionnum==5:
-        question=("it's two now")
+        question=("Who killed the high king and is almost to be excecuted in the 'unbound' intro before being interrupted by Aluin?")
         qa=q5
-        opt=("1. "
-             "2. "
-             "3. "
-             "4. ")
+        opt=("1. Legate Rikke        "
+             "2. Ulfric Stormcloak        "
+             "3. Galmar Stone-Fist        "
+             "4. Lydia Delphine")
 
 #checking the question number
 while questionnum<6:
@@ -91,4 +88,5 @@ while questionnum<6:
         print(anserror)
         
 #end message
-print("end")
+print("                                                    GOOD JOB!!!!! you completed the quiz!")
+print("                                                         your score was:   ", points,"/5")
